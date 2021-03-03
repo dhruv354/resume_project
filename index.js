06d6bd8fe1a0ec51ca2bd3f8537322e496764cf0
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-03-03 16:02:17 +0530

    added nodejs to the project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const port = 3000
+const path = require('path')
+
+
+var app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname , 'views'))
+
+//using middleware to include static files like css and javascript
+app.use(express.static('static'))
+app.get('/', (req, res) => {
+    return res.render('index')
+})
+
+app.listen(port, (err) => {
+    if(err){
+        console.log('Error found');
+        return res.send('Error')
+    }
+    console.log('Server running on port: ', port);
+})
