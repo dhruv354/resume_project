4b87a6620ca9a92595d2b179c39a82dbf6e1e24f
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-05-17 17:47:02 +0530

    final commit

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const port = 3000
-const path = require('path')
+const express = require("express");
+const port = process.env.port || 3000;
+const path = require("path");
 
-
-var app = express()
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname , 'views'))
+var app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //using middleware to include static files like css and javascript
-app.use(express.static('static'))
-app.get('/', (req, res) => {
-    return res.render('index')
-})
+app.use(express.static("static"));
+app.get("/", (req, res) => {
+  return res.render("index");
+});
 
 app.listen(port, (err) => {
-    if(err){
-        console.log('Error found');
-        return res.send('Error')
-    }
-    console.log('Server running on port: ', port);
-})
+  if (err) {
+    console.log("Error found");
+    return res.send("Error");
+  }
+  console.log("Server running on port: ", port);
+});
